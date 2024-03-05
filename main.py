a896668957188bae51a16ecdbe2ad7bc085d39f0
박병준 <77188449+AlpacaMale@users.noreply.github.com>
2024-03-05 16:46:32 +0900

    delete few lines

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ from app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-from flask import render_template
-def error(message):
-    return render_template("error.html", message=message)
